81c0aee293c4037bae6201237e334b72c68d7704
andrewroy3535 <eda.twinkle@gmail.com>
2022-06-28 11:20:28 +0600

    rendering text with blockContent

---
+++ src/views/GitDetails/GitDetails.js
@@ -0,0 +1,190 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  Button,
+  Animated,
+  FlatList,
+} from 'react-native';
+import React, {useContext} from 'react';
+const BlockContent = require('@sanity/block-content-to-react');
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {PortableText} from '@portabletext/react';
+import {Contextprovider} from '../../Context/Context';
+import FAB from '../../component/Fab/Fab';
+
+const GitDetails = ({route, navigation}) => {
+  const {item} = route.params;
+  const context = useContext(Contextprovider);
+  const {gitSonghita, goToNextBook, goToPreviousBook} = context;
+
+  // Scrolling amimation when scroll down
+  const srollHeightY = 100;
+  const scrollY = new Animated.Value(0);
+  const diffClampScrollY = Animated.diffClamp(scrollY, 0, srollHeightY);
+  const translateY = diffClampScrollY.interpolate({
+    inputRange: [0, srollHeightY],
+    outputRange: [0, srollHeightY],
+  });
+
+  const BlockRenderer = props => {
+    const {style = 'normal'} = props.node;
+
+    if (style === 'normal') {
+      return (
+        <View style={{backgroundColor: '#d3d3d3', padding: 15}}>
+          <Text style={{textAlign: 'center', fontSize: 19, color: '#171717'}}>
+            {props.children}
+          </Text>
+        </View>
+      );
+    }
+    if (style === 'h3') {
+      return (
+        <View style={{backgroundColor: '#d3d3d3', padding: 15}}>
+          <Text style={{textAlign: 'center', fontSize: 19, color: '#171717'}}>
+            {props.children}
+          </Text>
+        </View>
+      );
+    }
+    if (style === 'h1') {
+      return (
+        <View style={{backgroundColor: '#d3d3d3', padding: 15}}>
+          <Text style={{textAlign: 'center', fontSize: 19, color: '#171717'}}>
+            {props.children}
+          </Text>
+        </View>
+      );
+    }
+    if (style === 'h2') {
+      return (
+        <View style={{backgroundColor: '#d3d3d3', padding: 15}}>
+          <Text style={{textAlign: 'center', fontSize: 19, color: '#171717'}}>
+            {props.children}
+          </Text>
+        </View>
+      );
+    }
+    if (style === 'h4') {
+      return (
+        <View style={{backgroundColor: '#d3d3d3', padding: 15}}>
+          <Text style={{textAlign: 'center', fontSize: 19, color: '#171717'}}>
+            {props.children}
+          </Text>
+        </View>
+      );
+    }
+    if (style === 'h5') {
+      return (
+        <View style={{backgroundColor: '#d3d3d3', padding: 15}}>
+          <Text style={{textAlign: 'center', fontSize: 19, color: '#171717'}}>
+            {props.children}
+          </Text>
+        </View>
+      );
+    }
+    if (style === 'h6') {
+      return (
+        <View style={{backgroundColor: '#d3d3d3', padding: 15}}>
+          <Text style={{textAlign: 'center', fontSize: 19, color: '#171717'}}>
+            {props.children}
+          </Text>
+        </View>
+      );
+    }
+  };
+
+  // console.log(item);
+
+  return (
+    <>
+      <SafeAreaView>
+        <View style={styles.go_back_frm_books}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Gitsonghita')}
+            style={styles.touchable_opacity_back}>
+            <AntDesign name="left" size={25} color="#d3d3d3" />
+          </TouchableOpacity>
+          <View style={{width: '80%'}}>
+            <Text style={styles.headerText}>{item.title}</Text>
+          </View>
+        </View>
+        <View
+        // style={{
+        //   justifyContent: 'center',
+        //   alignItems: 'center',
+        // }}
+        >
+          <FlatList
+            onScroll={e => {
+              scrollY.setValue(e.nativeEvent.contentOffset.y);
+            }}
+            showsVerticalScrollIndicator={false}
+            data={item.bookDescription}
+            renderItem={item => (
+              <BlockContent
+                blocks={item.item}
+                serializers={{types: {block: BlockRenderer}}}
+              />
+            )}
+          />
+
+          {/* <ScrollView
+            showsVerticalScrollIndicator={false}
+            onScroll={e => {
+              scrollY.setValue(e.nativeEvent.contentOffset.y);
+            }}>
+            <BlockContent
+              blocks={item.bookDescription}
+              serializers={{types: {block: BlockRenderer}}}
+            />
+          </ScrollView> */}
+        </View>
+      </SafeAreaView>
+      <Animated.View
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 27,
+          alignItems: 'center',
+          transform: [{translateY}],
+        }}>
+        <FAB
+          goToNextBook={() =>
+            goToNextBook(gitSonghita, item, 'GitDetails', navigation)
+          }
+          goToPreviousBook={() =>
+            goToPreviousBook(gitSonghita, item, 'GitDetails', navigation)
+          }
+        />
+      </Animated.View>
+    </>
+  );
+};
+
+export default GitDetails;
+
+// styles array of objects
+const styles = StyleSheet.create({
+  go_back_frm_books: {
+    backgroundColor: '#171717',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 12,
+    color: '#d3d3d3',
+    alignSelf: 'center',
+  },
+  touchable_opacity_back: {
+    // borderWidth: 1,
+    width: '10%',
+    padding: 7,
+    borderRadius: 50,
+  },
+});
